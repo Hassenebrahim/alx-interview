7a9c0c561675e26c66fe58cb58b1d293547da57a
Hassenebrahim <99370403+Hassenebrahim@users.noreply.github.com>
2022-12-27 14:35:26 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-'''A module for working with Pascal's triangle.
-'''
+"""
+returns a list of lists of integers representing the Pascals triangle of n
+"""
 
 
 def pascal_triangle(n):
-    '''Creates a list of lists of integers representing
-    the Pascal's triangle of a given integer.
-    '''
-    if n <= 0:
-        return []
-    # First row/list of the triangle
-    final_list = [[1]]
-    # Loop the number of additional rows/lists needed.
-    # First list already added, number of new lists needed is n - 1
-    for x in range(n-1):
-        # Add zeros at the beginning and ending of last sublist in
-        # final_list(makes for easier addition for where values
-        # are non-existent)
-        temp = [0] + final_list[-1] + [0]
-        # Next list to be added to final_list
-        new_list = []
-        # Length of the next row (length of the last sublist + 1)
-        for y in range(len(final_list[-1]) + 1):
-            # Necessary addition on values of current row/list and
-            # appended to next list/row
-            new_list.append(temp[y] + temp[y+1])
-        # Append new row to final_list
-        final_list.append(new_list)
-    return final_list
+    """
+    returns a list of lists of integers
+    representing the Pascals triangle of n
+    """
 
+    if(n <= 0):
+        return []
+    else:
+        a = [[] for i in range(n)]
+        for i in range(n):
+            for j in range(i+1):
+                if(j < i):
+                    if(j == 0):
+                        a[i].append(1)
+                    else:
+                        a[i].append(a[i-1][j]+a[i-1][j-1])
+                elif(j == i):
+                    a[i].append(1)
+        return a
